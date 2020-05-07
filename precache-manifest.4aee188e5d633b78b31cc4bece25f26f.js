760676274017c90a1c63c76f3a2910f7f8564235
N White <c2lkn.t@gmail.com>
2020-05-07 17:43:51 -0500

    Updates

---
+++ precache-manifest.4aee188e5d633b78b31cc4bece25f26f.js
@@ -0,0 +1,450 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddb407795428a54b9a22f8aa0d1a022",
+    "url": "/digcoll/index.html"
+  },
+  {
+    "revision": "e238b430451a82ebb312",
+    "url": "/digcoll/static/css/2.c4b38aa0.chunk.css"
+  },
+  {
+    "revision": "8d73a182a860cbb4db2b",
+    "url": "/digcoll/static/css/main.76281d59.chunk.css"
+  },
+  {
+    "revision": "e238b430451a82ebb312",
+    "url": "/digcoll/static/js/2.86b32c13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digcoll/static/js/2.86b32c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d73a182a860cbb4db2b",
+    "url": "/digcoll/static/js/main.253e1065.chunk.js"
+  },
+  {
+    "revision": "b07e19665d5d4673023f",
+    "url": "/digcoll/static/js/runtime-main.6287391c.js"
+  },
+  {
+    "revision": "b6c11199dfffd4c1668a34275db4ac75",
+    "url": "/digcoll/static/media/0ca7dee8a005faa01f38fb529edad4f4.b6c11199.jpg"
+  },
+  {
+    "revision": "7850fdd29e11fe2b0a410771ee0e3fed",
+    "url": "/digcoll/static/media/1 - case_wing_d_522_25_w67_1917_no1_0.7850fdd2.jpg"
+  },
+  {
+    "revision": "0acdd85b4ec3d546697dc2f14813c87a",
+    "url": "/digcoll/static/media/150AyerMS1515_Escolios_1_r.0acdd85b.jpg"
+  },
+  {
+    "revision": "1b73d66f84f1cf756efb8da9fa84a735",
+    "url": "/digcoll/static/media/2353_25_w.1b73d66f.jpg"
+  },
+  {
+    "revision": "31e4e2828951883333e70ab8ed62c32b",
+    "url": "/digcoll/static/media/2_case_frc_15445_072-web1200-cropped.31e4e282.jpg"
+  },
+  {
+    "revision": "ca1f3bbae38803d20c6cf3be6c791745",
+    "url": "/digcoll/static/media/3.2.4.2-Case-W-999.182-Burnham,-plt.#51-Chicago-Looking-South-Over-Lagoons.ca1f3bba.jpg"
+  },
+  {
+    "revision": "982c96392e86e7a4cd771e66c5cc6e97",
+    "url": "/digcoll/static/media/450-treaty-of-prairie-du-ch.982c9639.jpg"
+  },
+  {
+    "revision": "ffa1a38532a407dec2d5fe41ee6ff9b4",
+    "url": "/digcoll/static/media/4a_7507_001_o2jpg.ffa1a385.jpg"
+  },
+  {
+    "revision": "4104f6baeae9b140d934461cbc17646b",
+    "url": "/digcoll/static/media/A 5 .315 vol.#9, Every Saturday, pg. 729, The Night Train, Nov.12,1870_w.4104f6ba.jpg"
+  },
+  {
+    "revision": "d0b05e92686c49eccd2502838c4a5f3c",
+    "url": "/digcoll/static/media/Ayer 1 C45 copy.d0b05e92.jpg"
+  },
+  {
+    "revision": "f9adcc29983b6dfb0585b95bab906ba1",
+    "url": "/digcoll/static/media/Ayer 6 .P9 1482a (VAULT), Cosmographia, Ptolemy, World Map_w.f9adcc29.jpg"
+  },
+  {
+    "revision": "aa4350cd981e4eabe3bb7aa1175d33ec",
+    "url": "/digcoll/static/media/Ayer 655.51.C8 1524b (Vault) Praeclara Ferdina di Cortesii de noua, Cortes Map_w(1).aa4350cd.jpg"
+  },
+  {
+    "revision": "ef5497c836f7dcb6cd2b323567915ad2",
+    "url": "/digcoll/static/media/Ayer-MS-Map-30-sheet-no.106,-Veue-de-la-ville-des-Trois-Rivieres-en-Canada,-Nouvelle-France,-1721_0.ef5497c8.jpg"
+  },
+  {
+    "revision": "fbbc50a62c6c45df58dd8ffbb7e597d1",
+    "url": "/digcoll/static/media/CA2 logo 4.fbbc50a6.jpg"
+  },
+  {
+    "revision": "1fec13799f56392da5089c3fa8dff8cb",
+    "url": "/digcoll/static/media/CBQGR206_w(1).1fec1379.jpg"
+  },
+  {
+    "revision": "ebafcee6f7f115a724da069ba05eadaf",
+    "url": "/digcoll/static/media/Capture.ebafcee6.jpg"
+  },
+  {
+    "revision": "9b6f81cbde900d981493a96158229f30",
+    "url": "/digcoll/static/media/Capture_0.9b6f81cb.jpg"
+  },
+  {
+    "revision": "e6af77c673ada17ed44b94397d5f2713",
+    "url": "/digcoll/static/media/Case BV4500 .W65 1624,Seven Goulden Candlesticks.e6af77c6.jpg"
+  },
+  {
+    "revision": "32d7b22676fdade92b7b2cdb160cc6d5",
+    "url": "/digcoll/static/media/Case E468.7.M96 1890 (VAULT), The Myriopticon -2_o3.32d7b226.jpg"
+  },
+  {
+    "revision": "2626cb0ba703cfdfa6382f1f8183b661",
+    "url": "/digcoll/static/media/Case F 4549 .137, Annales the true and royal history, Queen Elizabeth, frontis_w.2626cb0b.jpg"
+  },
+  {
+    "revision": "245fbe6cdd1d34f97a112ae47e3f5ff0",
+    "url": "/digcoll/static/media/Case MS 55.5 (Vault), Le ieu des echez moralisiet que fit vn de noz freres, folio. 5r_w.245fbe6c.jpg"
+  },
+  {
+    "revision": "de52dced14f5590b5906c203a5b8eeb8",
+    "url": "/digcoll/static/media/Case N 381.761 V1.de52dced.jpg"
+  },
+  {
+    "revision": "9966ac5220690b3a4c353729119b805f",
+    "url": "/digcoll/static/media/Case R 1832.07 (Oversize) Book of the Fair, Ferris Wheel Midway Plaisance_o3_0.9966ac52.jpg"
+  },
+  {
+    "revision": "59e0cf820a2bd0958b0b4c6821db1567",
+    "url": "/digcoll/static/media/Case U 26 .675 (oversize), Des fortifications et artifices architecture, psalm 91_w.59e0cf82.jpg"
+  },
+  {
+    "revision": "3ce6d575a595bab9b8281344ddcc8e6f",
+    "url": "/digcoll/static/media/Case Wing DC 137.08.F73 vol.6 no.3 Le Menage_w.3ce6d575.jpg"
+  },
+  {
+    "revision": "5787fe6b46a686be03794a4249cef494",
+    "url": "/digcoll/static/media/Case YS01 First Folio, Shakespreare, Title page, frontispiece_o2(1).5787fe6b.jpg"
+  },
+  {
+    "revision": "f7635df315dfc20eaa2d2bd3a88597cc",
+    "url": "/digcoll/static/media/Clarke Family-juggling_w.f7635df3.jpg"
+  },
+  {
+    "revision": "7c160c293096c43d53db4534c8acb28e",
+    "url": "/digcoll/static/media/Deal_B1_F3_P1.7c160c29.jpg"
+  },
+  {
+    "revision": "5a30194259704da5e1ac0475b6e579fd",
+    "url": "/digcoll/static/media/Driscoll Bx.161, Fl.2, National Melodies-Star Spangled Banner_w(1).5a301942.jpg"
+  },
+  {
+    "revision": "4847f956e6fd2bca852b895f22628f6c",
+    "url": "/digcoll/static/media/Driscoll Bx.206, Series 9, Sheet Music, Cover-#034_o3.4847f956.jpg"
+  },
+  {
+    "revision": "bd0ee7a7ee2aa99f099724572a7fe832",
+    "url": "/digcoll/static/media/Elisabeth_certificate_December_20_1764.bd0ee7a7.jpg"
+  },
+  {
+    "revision": "81b814b058004f7f74afc26f49cd4f88",
+    "url": "/digcoll/static/media/Graff 2493 (Vault) Photograph of Abraham Lincoln, 1859_w_0.81b814b0.jpg"
+  },
+  {
+    "revision": "e41e644280cc8f642e4239948f8b0f9b",
+    "url": "/digcoll/static/media/Happy_Days.e41e6442.jpg"
+  },
+  {
+    "revision": "d79081e0e28e57c9d18470213b3e93b9",
+    "url": "/digcoll/static/media/Indians_Midwest.d79081e0.jpg"
+  },
+  {
+    "revision": "4347601977af8ba6ba0f29a099752dc7",
+    "url": "/digcoll/static/media/MMS Pullman 13-01-01 Box#16 Folder#723, Pullman Car Interior_w.43476019.jpg"
+  },
+  {
+    "revision": "fa7a81a25acb0331ac78f75e748f51ff",
+    "url": "/digcoll/static/media/Map6F-G4104.C6E2-1895-.G7,-sheet#2,-Chicago-Nationalities-Map.fa7a81a2.jpg"
+  },
+  {
+    "revision": "ffbfcad748b3bd6fe894b428c6634910",
+    "url": "/digcoll/static/media/Midwest MS Dill Pickle Bx.1 Fl.#81, Lending Library_w.ffbfcad7.jpg"
+  },
+  {
+    "revision": "3ad28a42ac39a0e8c874fb57ceea2e1b",
+    "url": "/digcoll/static/media/Myrtle_Livers_on_porch_of_home_in_Zion_Hill_Kentucky_circa_1935.3ad28a42.jpg"
+  },
+  {
+    "revision": "50ffc0319680f8477853f94e47924ad0",
+    "url": "/digcoll/static/media/NL Archives 15-01-01 Bx.#1, Everett D. Graff_w(1).50ffc031.jpg"
+  },
+  {
+    "revision": "11d87e7fd852fc1e516f51c5ac185b57",
+    "url": "/digcoll/static/media/NL000477.11d87e7f.jpg"
+  },
+  {
+    "revision": "304ce55450843ae20ae86123e4653042",
+    "url": "/digcoll/static/media/NL005174_o4_cropped.304ce554.jpg"
+  },
+  {
+    "revision": "ca6ebcc9cc88c27cf96f1c8a2586e118",
+    "url": "/digcoll/static/media/NL_CasePullman_09_00_02_1_55_0 copy.ca6ebcc9.jpg"
+  },
+  {
+    "revision": "7e7f15e25b824cee3b3fa614b4b676fc",
+    "url": "/digcoll/static/media/NL_VAULT_oversize_Ayer_MS_map_26_0048_o3.7e7f15e2.jpg"
+  },
+  {
+    "revision": "7306ddd082587319f74e56a44a06de8c",
+    "url": "/digcoll/static/media/Newberry-Library,-Henry-Ives-Cobb,-Painting_0.7306ddd0.jpg"
+  },
+  {
+    "revision": "87ab1014c313f1395c72de036109f793",
+    "url": "/digcoll/static/media/Polyglot(1).87ab1014.jpg"
+  },
+  {
+    "revision": "3489bee63998432b6545f6d27a0ecc64",
+    "url": "/digcoll/static/media/RCandP image.3489bee6.jpg"
+  },
+  {
+    "revision": "9cf84f9287440c9f15854bcae9d51a6a",
+    "url": "/digcoll/static/media/So-Long-Sammy-cover.9cf84f92.jpg"
+  },
+  {
+    "revision": "d1d664ac2bc3841f6de75f3f01a706e9",
+    "url": "/digcoll/static/media/Thumbnail1165x794MexicanHometownAssociations.d1d664ac.jpg"
+  },
+  {
+    "revision": "4647915499cb7b411b658d31f4a928ea",
+    "url": "/digcoll/static/media/United_States_of_America_compiled_from_the_latest__best_authorities(1).46479154.jpg"
+  },
+  {
+    "revision": "61dfa82f72d616cc2502d811d91638bb",
+    "url": "/digcoll/static/media/Venice map(1).61dfa82f.jpg"
+  },
+  {
+    "revision": "00921f21270725891565422e5a9b55ae",
+    "url": "/digcoll/static/media/White_City_Mag.00921f21.jpg"
+  },
+  {
+    "revision": "43dec12ea8b961fab202028e094377f8",
+    "url": "/digcoll/static/media/Wing ZP 535 .A354 (Vault), Divina commedia, title page.43dec12e.jpg"
+  },
+  {
+    "revision": "f6294ab25874ea7b27369252c9dc99c3",
+    "url": "/digcoll/static/media/Woman_on_black_horse_greets_man(1).f6294ab2.jpg"
+  },
+  {
+    "revision": "d65a81507b12eb87b6e8ad93bf72e360",
+    "url": "/digcoll/static/media/ae6c6ff4206252eb13cf0e3c7690d9d4_0.d65a8150.jpg"
+  },
+  {
+    "revision": "b91fed971b2eb60fcc369129341fc65b",
+    "url": "/digcoll/static/media/ahcbp_ok.b91fed97.jpg"
+  },
+  {
+    "revision": "43bb6f0e4fd8d0e1ad19d4a7e3ea9955",
+    "url": "/digcoll/static/media/anonymous-pro-latin-400.43bb6f0e.woff2"
+  },
+  {
+    "revision": "95cb6fa32827a858c3878dd8779fb542",
+    "url": "/digcoll/static/media/anonymous-pro-latin-400.95cb6fa3.woff"
+  },
+  {
+    "revision": "b4a43e4f9384a02e7e286841285f3142",
+    "url": "/digcoll/static/media/anonymous-pro-latin-400italic.b4a43e4f.woff"
+  },
+  {
+    "revision": "eb2ee7d3973d92e35bba3c11240b9dea",
+    "url": "/digcoll/static/media/anonymous-pro-latin-400italic.eb2ee7d3.woff2"
+  },
+  {
+    "revision": "6ea4bda30de8b0217f88b9aa7122fe22",
+    "url": "/digcoll/static/media/anonymous-pro-latin-700.6ea4bda3.woff"
+  },
+  {
+    "revision": "d8adbd3e58bb6f2fa546e44b42254b9e",
+    "url": "/digcoll/static/media/anonymous-pro-latin-700.d8adbd3e.woff2"
+  },
+  {
+    "revision": "2f6c5b668610a8203f077e05dd8d814a",
+    "url": "/digcoll/static/media/anonymous-pro-latin-700italic.2f6c5b66.woff"
+  },
+  {
+    "revision": "32871a8a15ef3679113f0937dc30ae43",
+    "url": "/digcoll/static/media/anonymous-pro-latin-700italic.32871a8a.woff2"
+  },
+  {
+    "revision": "5d5ef51c124ceede14a8a8447d29e2aa",
+    "url": "/digcoll/static/media/ba678715d92078fd584ed630478d8b5e.5d5ef51c.jpg"
+  },
+  {
+    "revision": "9d3e2a656da38050902f884540e615e2",
+    "url": "/digcoll/static/media/bannersky.9d3e2a65.jpg"
+  },
+  {
+    "revision": "76d8622e7312601e962f99e8d61178e1",
+    "url": "/digcoll/static/media/bear_0001_w.76d8622e.jpg"
+  },
+  {
+    "revision": "6835e5cf126b1c850035f9e64c5a42f7",
+    "url": "/digcoll/static/media/bible-in-print-screen.6835e5cf.jpg"
+  },
+  {
+    "revision": "a98e04dd63e24a5f98ea6a02111f13a0",
+    "url": "/digcoll/static/media/c0bf56d04aa168eaf80bdb508eafb010.a98e04dd.jpg"
+  },
+  {
+    "revision": "11eb5f61157679e2160b30cf21d5e37d",
+    "url": "/digcoll/static/media/case_minus_vm_1579_b99p_sig_a2v_b1r_o2.11eb5f61.jpg"
+  },
+  {
+    "revision": "b9831aa1e935bb462243bf5cce69b6d6",
+    "url": "/digcoll/static/media/case_ms_5017_017_o2 for portal.b9831aa1.jpg"
+  },
+  {
+    "revision": "c74f0ced40384a51d4b66f47f895c8de",
+    "url": "/digcoll/static/media/case_wing_zc_93_185_fl_052_005_o2 for portal_0.c74f0ced.jpg"
+  },
+  {
+    "revision": "e599c244928873eac4adf88ba511bd17",
+    "url": "/digcoll/static/media/censier.e599c244.jpg"
+  },
+  {
+    "revision": "0ccc144cf238f9b6c4f30b849ebdd234",
+    "url": "/digcoll/static/media/cgsq20.0ccc144c.jpg"
+  },
+  {
+    "revision": "5f4c2642349ec15e3be4e3d3058dc46d",
+    "url": "/digcoll/static/media/cinzel-decorative-latin-400.5f4c2642.woff"
+  },
+  {
+    "revision": "ca088199890d37753a2e50910057d221",
+    "url": "/digcoll/static/media/cinzel-decorative-latin-400.ca088199.woff2"
+  },
+  {
+    "revision": "168bc30508cfe169e1021c6afe83e2d4",
+    "url": "/digcoll/static/media/cinzel-decorative-latin-700.168bc305.woff"
+  },
+  {
+    "revision": "1c3f24d4067a3f29440b41737bfdcaf7",
+    "url": "/digcoll/static/media/cinzel-decorative-latin-700.1c3f24d4.woff2"
+  },
+  {
+    "revision": "41a8af98c892194f452b2a928074c9ac",
+    "url": "/digcoll/static/media/cinzel-decorative-latin-900.41a8af98.woff2"
+  },
+  {
+    "revision": "9e9a8be4ee4fd7218a9ce9a2ca28e3c8",
+    "url": "/digcoll/static/media/cinzel-decorative-latin-900.9e9a8be4.woff"
+  },
+  {
+    "revision": "94b3e851624088a59ea7028526d6903c",
+    "url": "/digcoll/static/media/dd6eba0948f3cef2a500fa39390cebb8_0.94b3e851.jpg"
+  },
+  {
+    "revision": "08d41541184328cd61b9bd884229d43d",
+    "url": "/digcoll/static/media/declarationdesdr00phil_0001.08d41541.jpg"
+  },
+  {
+    "revision": "5f474bd492f026276b147e25b549a3dd",
+    "url": "/digcoll/static/media/driscoll_section_6_box_164_fl_f_france_we_have_not_forgotten_0000.5f474bd4.jpg"
+  },
+  {
+    "revision": "44ffbf098b78502c7dd69ff4dc51fcbc",
+    "url": "/digcoll/static/media/flpsdante.44ffbf09.jpg"
+  },
+  {
+    "revision": "85bc1ad4fc0694b95fe1100a0c0869e9",
+    "url": "/digcoll/static/media/geo index screenshot.85bc1ad4.jpg"
+  },
+  {
+    "revision": "f1a53df5e76508412f8c7a47af324749",
+    "url": "/digcoll/static/media/jm249(1).f1a53df5.jpg"
+  },
+  {
+    "revision": "7c99807f482cd521da49ade67ce7d74d",
+    "url": "/digcoll/static/media/luther-controversy_0.7c99807f.jpg"
+  },
+  {
+    "revision": "d7c42a235b49bca113a2c38fc509b290",
+    "url": "/digcoll/static/media/major-mono-display-latin-400.d7c42a23.woff2"
+  },
+  {
+    "revision": "fcf51039243738878dd22044a831103d",
+    "url": "/digcoll/static/media/major-mono-display-latin-400.fcf51039.woff"
+  },
+  {
+    "revision": "849db2ed0f94d58a7c88d83f79434953",
+    "url": "/digcoll/static/media/meda songs.849db2ed.jpg"
+  },
+  {
+    "revision": "5f08b972add97b420f1e142791acd483",
+    "url": "/digcoll/static/media/men_pulling_zephyr.5f08b972.jpg"
+  },
+  {
+    "revision": "8d963ac99446f7b3c870db8db91a1bec",
+    "url": "/digcoll/static/media/mms_jannotta_fl_hair_2pg_001_005_o3.8d963ac9.jpg"
+  },
+  {
+    "revision": "01c57eadc301e998ed332b0d10756f06",
+    "url": "/digcoll/static/media/rmcn-atlas-a564-1914,-atlas-of-mexican-conflict,-1914_resize_1f242fc0e1.01c57ead.jpg"
+  },
+  {
+    "revision": "121353b2da64d4932827cd7f2fb8412e",
+    "url": "/digcoll/static/media/rr.121353b2.jpg"
+  },
+  {
+    "revision": "bbc6d1c1c67d02be47ae3d2d17eba2de",
+    "url": "/digcoll/static/media/sc000034_8a2657d731.bbc6d1c1.jpg"
+  },
+  {
+    "revision": "70861dbcb9df07419fb31b3a67a8d303",
+    "url": "/digcoll/static/media/sc1301, Emblemata politica..Curiae Noribergensis depicta, emblem Minor esca maioris_o3.70861dbc.jpg"
+  },
+  {
+    "revision": "9ae779e69687b7a70ba2a6dc9f18afd2",
+    "url": "/digcoll/static/media/size(2).9ae779e6.jpg"
+  },
+  {
+    "revision": "a6dfa732aff618427ad45f3bc51891ad",
+    "url": "/digcoll/static/media/size.a6dfa732.jpg"
+  },
+  {
+    "revision": "a29073532d3166b72d9ffb5d7bcef2b8",
+    "url": "/digcoll/static/media/terence-sm_0.a2907353.jpg"
+  },
+  {
+    "revision": "272b7087013a9b184dcd9cafe7acf21d",
+    "url": "/digcoll/static/media/the bokums.272b7087.jpg"
+  },
+  {
+    "revision": "0e8b03924dda7325cebeb08c558bf04f",
+    "url": "/digcoll/static/media/the-newberry-notag.0e8b0392.png"
+  },
+  {
+    "revision": "d92a788fc6c43af78d858d17d261764d",
+    "url": "/digcoll/static/media/the-newberry.d92a788f.png"
+  },
+  {
+    "revision": "b6d47c13182ec608f099d990b2bb6f39",
+    "url": "/digcoll/static/media/vast-shadow-latin-400.b6d47c13.woff2"
+  },
+  {
+    "revision": "f1420767b32861f98ca261ac9d999ddc",
+    "url": "/digcoll/static/media/vast-shadow-latin-400.f1420767.woff"
+  },
+  {
+    "revision": "a92556e80d9e9943cdee1d9b18dfd086",
+    "url": "/digcoll/static/media/voices-rcportal.a92556e8.jpg"
+  },
+  {
+    "revision": "c05d2677ce5f811e1af86c474e47d49e",
+    "url": "/digcoll/static/media/ys_62_62_c_o2.c05d2677.jpg"
+  }
+]);
